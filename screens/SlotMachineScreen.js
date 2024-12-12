e96537778c7fdafaeb30303cff7461d3d8f4e1f8
ErickGHZ <1722110095@utectulancingo.edu.mx>
2024-12-12 11:43:13 -0600

    Refactor SlotMachineScreen to include XP animation and update inventory

--- screens/SlotMachineScreen.js
+++ screens/SlotMachineScreen.js
@@ -130,15 +130,26 @@ export default function SlotMachine({ navigation }) {
       let nextLevelXP = calculateNextLevelXP(currentLevel); // XP necesario para el siguiente nivel
   
       if (outcome === '¡Ganaste!') {
-        updatedExp += Math.floor(betAmount / 10); // XP basado en la apuesta
+        const xpGained = Math.floor(betAmount / 10);
+        const newExp = inventory.exp + xpGained;
+        animateXP(inventory.exp, newExp);
+
+        updatedExp += xpGained;
+
         const newCoins = updatedInventory.coins + winnings;
+        
   
         // Agregar animación para sumar monedas
         animateCoins(updatedInventory.coins, newCoins);
   
         updatedInventory.coins = newCoins; // Ganancia de monedas
       } else {
-        updatedExp += Math.floor(betAmount / 10); // XP basado en la apuesta
+        const xpGained = Math.floor(betAmount / 10);
+        const newExp = inventory.exp + xpGained;
+        animateXP(inventory.exp, newExp);
+
+        updatedExp += xpGained;
+
       }
   
       // Actualizamos el inventario con la nueva XP y monedas
@@ -232,9 +243,26 @@ export default function SlotMachine({ navigation }) {
       setInventory((prev) => ({ ...prev, coins: Math.round(current) }));
     }, interval);
   };
+  const animateXP = (start, end, duration = 100) => {
+    const difference = end - start;
+    const step = difference / 20; // Número de pasos
+    let current = start;
+    const interval = duration / 20; // Duración total dividida entre pasos
   
-
- 
+    const intervalId = setInterval(() => {
+      current += step;
+      if (current >= end) {
+        current = end; // Asegúrate de no pasar del valor final
+        clearInterval(intervalId);
+      }
+      setInventory((prev) => ({
+        ...prev,
+        exp: Math.round(current),
+      }));
+    }, interval);
+  };
+  
+  
 
   return (
     <View style={styles.container}>
